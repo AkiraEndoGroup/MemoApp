17fe14a9145c9b71e9a64696c5a84f12bf5fb72a
Akira Endo <akira.e.aendo.group@gmail.com>
2018-06-22 16:10:51 +0900

    Created firebase certificate

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
+import firebase from 'firebase';
 import { createStackNavigator } from 'react-navigation';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
+
+import env from './env.json';
+
+const config = {
+  apiKey: env.FIREBASE_API_KEY,
+  authDomain: env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: env.FIREBASE_DB_URL,
+  projectId: env.FIREBASE_PRJ_ID,
+  storageBucket: env.FIREBASE_STORAGE,
+  messagingSenderId: env.FIREBASE_SENDER_ID,
+};
+firebase.initializeApp(config);
 
 const App = createStackNavigator({
   Login:      { screen: LoginScreen },
