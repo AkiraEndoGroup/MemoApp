0ab1b84aee0d85e0fd6f47ea92003e4a73da9ae5
Akira Endo <akira.e.aendo.group@gmail.com>
2018-06-24 00:44:06 +0900

    Implemented Delete function.

--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableHighlight, FlatList } from 'react-native';
+import Swipeout from 'rc-swipeout';
+import firebase from 'firebase';
 
 const dateString = (date) => {
   if (date == null) { return ''; }
@@ -8,18 +10,51 @@ const dateString = (date) => {
 };
 
 class MemoList extends React.Component {
+  deleteNote(key: string) {
+    const { currentUser } = firebase.auth();
+    const db = firebase.firestore();
+    db.collection(`users/${currentUser.uid}/memos`).doc(key.key).delete()
+      .then(() => {
+      })
+      .catch(() => {
+      });
+  }
+
   renderMemo({ item }) {
     return (
-      <TouchableHighlight onPress={() => { this.props.navigation.navigate('MemoDetail', { memo: item }); }}>
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>{item.body.substring(0, 10)}</Text>
-          <Text style={styles.memoDate}>{dateString(item.createdOn)}</Text>
-        </View>
-      </TouchableHighlight>
+      <Swipeout
+        right={[
+        {
+          text: 'delete',
+          onPress:() => this.deleteNote({ key: item.key }),
+          style: { backgroundColor: 'red', color: '#fff' },
+          className: 'custom-class-1',
+          backgroundColor: '#fff',
+        },
+        ]}
+        onOpen={() => {}}
+        onClose={() => {}}
+      >
+        <TouchableHighlight onPress={() => { this.props.navigation.navigate('MemoDetail', { memo: item }); }}>
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>{item.body.substring(0, 10)}</Text>
+            <Text style={styles.memoDate}>{dateString(item.createdOn)}</Text>
+          </View>
+        </TouchableHighlight>
+      </Swipeout>
     );
   }
 
   render() {
+    // const swipeBtns = [
+    //   {
+    //     text: 'Delete',
+    //     backgroundColor: 'red',
+    //     underlayColor: 'rgba(0, 0, 0, 1, 0.6)',
+    //     onPress: () => { this.deleteNote(this.state.memoList[0]); }
+    //   },
+    // ];
+
     return (
       <View style={styles.memoList}>
         <FlatList
@@ -51,6 +86,5 @@ const styles = StyleSheet.create({
     color: '#a2a2a2',
   },
 });
-
 
 export default MemoList;
